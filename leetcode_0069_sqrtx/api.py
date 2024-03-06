d4aa82d0ee4141f9bd6b03403cf01aade4433c03
Andy Xie <andy.xie.opensource@gmail.com>
2024-03-06 19:39:31 +0800

    api.py: Specify arg

--- leetcode_0069_sqrtx/api.py
+++ leetcode_0069_sqrtx/api.py
@@ -1,12 +1,13 @@
 """API for solving problem Sqrt(x)"""
 
-def _check_preconditions(...) -> bool:
+
+def _check_preconditions(x) -> bool:
     pass
 
 
-def sqrtx(...) -> ...:
+def sqrtx(x) -> int:
     """Solves problem Sqrt(x)"""
 
-    assert _check_preconditions(...)
+    assert _check_preconditions(x)
 
     pass
