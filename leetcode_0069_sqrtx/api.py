fcd0e97c2ed93f179e66c1080b074bc0d0c809ec
Andy Xie <andy.xie.opensource@gmail.com>
2024-03-09 17:30:33 +0800

    api.py: Fix swapped logic

--- leetcode_0069_sqrtx/api.py
+++ leetcode_0069_sqrtx/api.py
@@ -8,7 +8,7 @@ def _check_preconditions(x: int) -> bool:
     if x < 0:
         return False
 
-    if X_MIN <= x <= X_MAX:
+    if not X_MIN <= x <= X_MAX:
         return False
 
     return True
