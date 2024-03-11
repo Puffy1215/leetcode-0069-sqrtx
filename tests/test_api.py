891cf0654202d415b5f91008a8e11a936dca69b4
Andy Xie <andy.xie.opensource@gmail.com>
2024-03-11 19:38:21 +0800

    test_api.py: Implement x_rand()

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,7 @@
 """Tests API for solving problem Sqrt(x)"""
 
+import random
+
 import pytest
 
 from leetcode_0069_sqrtx import api
@@ -20,3 +22,11 @@ def test_sqrtx(result: int, x: int) -> None:
     """Tests solution for problem Sqrt(x)"""
 
     assert api.sqrtx(x) == result
+
+
+@pytest.fixture
+def x_rand():
+    def _x_rand():
+        return random.randint(api.X_MIN, api.X_MAX)
+
+    return _x_rand
