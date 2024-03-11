8e928d343c5cf8cb3ffd9e97934981356580c528
Andy Xie <andy.xie.opensource@gmail.com>
2024-03-11 17:36:01 +0800

    test_api.py: More test data

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,8 +8,12 @@ from leetcode_0069_sqrtx import api
 @pytest.mark.parametrize(
     ["result", "x"],
     (
+        [0, 0],
+        [1, 1],
         [2, 4],
         [2, 8],
+        [3, 9],
+        [3, 10],
     ),
 )
 def test_sqrtx(result: int, x: int) -> None:
